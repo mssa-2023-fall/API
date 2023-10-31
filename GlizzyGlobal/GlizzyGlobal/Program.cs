48b57a7710f5a5ff332bf70ee6bed68a45bb94e0
BbyWiz <96n_lawrence@outlook.com>
2023-10-31 16:02:29 -0400

    New comments on line 10/Program.cs

--- GlizzyGlobal/GlizzyGlobal/Program.cs
+++ GlizzyGlobal/GlizzyGlobal/Program.cs
@@ -3,14 +3,14 @@ using MortgageCalculatorLibrary;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Adding services to the container.
 builder.Services.AddControllers();
 
-// Register the currency conversion services.
+//Waiting for MortgageCalculatorLibrary 1.1 Release, build error for now.
 builder.Services.AddSingleton<ICurrencyRateProvider, CurrencyServiceRateProvider>();
 builder.Services.AddSingleton<ICurrencyConverter, CurrencyConverter>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle //NO
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
